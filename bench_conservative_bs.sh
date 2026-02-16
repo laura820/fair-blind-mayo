97de4ad8aacd9a954c0656fe2fe73ad1d0d7ce36
Shibam Mukherjee <shimuk8@gmail.com>
2026-02-16 17:51:14 +0100

    bench script modified slightly for better logging

--- bench_conservative_bs.sh
+++ bench_conservative_bs.sh
@@ -8,14 +8,14 @@ cd vole
 chmod +x clean.sh
 ./clean.sh 
 chmod +x build_consv_bs_keccak.sh
-./build_consv_bs_keccak.sh > ../bench_log_misc.txt
+./build_consv_bs_keccak.sh >> ../bench_log_misc.txt 2>&1
 cd ..
 cd vole-keccak-then-mayo-sys
 cargo clean
 cd ..
 cd blind-signatures-conservative
 cargo clean
-cargo test test_and_bench_sign_loop_conservative_128sv1 --no-run > ../bench_log_misc.txt
+cargo test test_and_bench_sign_loop_conservative_128sv1 --no-run >> ../bench_log_misc.txt 2>&1
 RUST_MIN_STACK=9999999999 cargo test test_and_bench_sign_loop_conservative_128sv1 -- --nocapture | sed -n '/MAYO/p' >> ../bench_log.txt
 RUST_MIN_STACK=9999999999 cargo test test_and_bench_sign_loop_conservative_128fv1 -- --nocapture | sed -n '/MAYO/p' >> ../bench_log.txt
 
@@ -33,14 +33,14 @@ RUST_MIN_STACK=9999999999 cargo test test_and_bench_sign_loop_conservative_256fv
 # # Getting conservative deg-16 bench
 # ./clean.sh 
 # chmod +x build_consv_bs_keccak_deg16.sh
-# ./build_consv_bs_keccak_deg16.sh > ../bench_log_misc.txt
+# ./build_consv_bs_keccak_deg16.sh >> ../bench_log_misc.txt 2>&1
 # cd ..
 # cd vole-keccak-deg16-then-mayo-sys
 # cargo clean
 # cd ..
 # cd blind-signatures-conservative-deg16
 # cargo clean
-# cargo test test_and_bench_sign_loop_conservative_128sv1 --no-run > ../bench_log_misc.txt
+# cargo test test_and_bench_sign_loop_conservative_128sv1 --no-run >> ../bench_log_misc.txt 2>&1
 # RUST_MIN_STACK=9999999999 cargo test test_and_bench_sign_loop_conservative_128sv1 -- --nocapture | sed -n '/MAYO/p' >> ../bench_log.txt
 # RUST_MIN_STACK=9999999999 cargo test test_and_bench_sign_loop_conservative_128fv1 -- --nocapture | sed -n '/MAYO/p' >> ../bench_log.txt
 # echo "--------------------------------------------------------" >> ../bench_log.txt
@@ -57,14 +57,14 @@ RUST_MIN_STACK=9999999999 cargo test test_and_bench_sign_loop_conservative_256fv
 # # Getting conservative rainhash bench
 # ./clean.sh 
 # chmod +x build_consv_bs_rainhash.sh
-# ./build_consv_bs_rainhash.sh > ../bench_log_misc.txt
+# ./build_consv_bs_rainhash.sh >> ../bench_log_misc.txt 2>&1
 # cd ..
 # cd vole-rainhash-then-mayo-sys
 # cargo clean
 # cd ..
 # cd blind-signatures-conservative-rain
 # cargo clean
-# cargo test test_and_bench_sign_loop_conservative_rain_128sv1 --no-run > ../bench_log_misc.txt
+# cargo test test_and_bench_sign_loop_conservative_rain_128sv1 --no-run >> ../bench_log_misc.txt 2>&1
 # RUST_MIN_STACK=9999999999 cargo test test_and_bench_sign_loop_conservative_rain_128sv1 -- --nocapture | sed -n '/MAYO/p' >> ../bench_log.txt
 # RUST_MIN_STACK=9999999999 cargo test test_and_bench_sign_loop_conservative_rain_128fv1 -- --nocapture | sed -n '/MAYO/p' >> ../bench_log.txt
 
@@ -75,14 +75,14 @@ RUST_MIN_STACK=9999999999 cargo test test_and_bench_sign_loop_conservative_256fv
 # # Getting optimized bench
 # ./clean.sh 
 # chmod +x build_opti_bs.sh
-# ./build_opti_bs.sh > ../bench_log_misc.txt
+# ./build_opti_bs.sh >> ../bench_log_misc.txt 2>&1
 # cd ..
 # cd vole-mayo-sys
 # cargo clean
 # cd ..
 # cd blind-signatures
 # cargo clean
-# cargo test test_and_bench_sign_loop_optimized_128sv1 --no-run > ../bench_log_misc.txt
+# cargo test test_and_bench_sign_loop_optimized_128sv1 --no-run >> ../bench_log_misc.txt 2>&1
 # RUST_MIN_STACK=9999999999 cargo test test_and_bench_sign_loop_optimized_128sv1 -- --nocapture | sed -n '/MAYO/p' >> ../bench_log.txt
 # RUST_MIN_STACK=9999999999 cargo test test_and_bench_sign_loop_optimized_128fv1 -- --nocapture | sed -n '/MAYO/p' >> ../bench_log.txt
 # echo "--------------------------------------------------------" >> ../bench_log.txt
